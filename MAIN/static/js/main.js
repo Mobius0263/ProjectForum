43f568ad52dea016913d8a195ddd387fae047bf5
Mobis0263 <mahendra33115@gmail.com>
2024-07-02 23:49:08 +0700

    Updated js

--- MAIN/static/js/main.js
+++ MAIN/static/js/main.js
@@ -1,26 +1,27 @@
-//NavBar
+// Function to hide the icon bar and show the navigation menu
 function hideIconBar(){
-    var iconBar = document.getElementById("iconBar");
-    var navigation = document.getElementById("navigation");
-    iconBar.setAttribute("style", "display:none;");
-    navigation.classList.remove("hide");
+    var iconBar = document.getElementById("iconBar");  // Get the element with ID 'iconBar'
+    var navigation = document.getElementById("navigation");  // Get the element with ID 'navigation'
+    iconBar.setAttribute("style", "display:none;");  // Set the display style of 'iconBar' to none (hide it)
+    navigation.classList.remove("hide");  // Remove the 'hide' class from the 'navigation' element (show it)
 }
 
+// Function to show the icon bar and hide the navigation menu
 function showIconBar(){
-    var iconBar = document.getElementById("iconBar");
-    var navigation = document.getElementById("navigation");
-    iconBar.setAttribute("style", "display:block;");
-    navigation.classList.add("hide");
+    var iconBar = document.getElementById("iconBar");  // Get the element with ID 'iconBar'
+    var navigation = document.getElementById("navigation");  // Get the element with ID 'navigation'
+    iconBar.setAttribute("style", "display:block;");  // Set the display style of 'iconBar' to block (show it)
+    navigation.classList.add("hide");  // Add the 'hide' class to the 'navigation' element (hide it)
 }
 
-//Comment
+// Function to show the comment area
 function showComment(){
-    var commentArea = document.getElementById("comment-area");
-    commentArea.classList.remove("hide");
+    var commentArea = document.getElementById("comment-area");  // Get the element with ID 'comment-area'
+    commentArea.classList.remove("hide");  // Remove the 'hide' class from the 'comment-area' element (show it)
 }
 
-//Reply
+// Function to show the reply area
 function showReply(){
-    var replyArea = document.getElementById("reply-area");
-    replyArea.classList.remove("hide");
+    var replyArea = document.getElementById("reply-area");  // Get the element with ID 'reply-area'
+    replyArea.classList.remove("hide");  // Remove the 'hide' class from the 'reply-area' element (show it)
 }
